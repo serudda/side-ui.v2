fd5e81753cd0ccb99f305e1568a3830c12b96c08
serudda <sergioruizdavila@gmail.com>
2025-08-28 14:31:53 -0500

    feat(preview): upgrade Storybook to v9.1.3 and update configurations

--- apps/preview/.storybook/main.ts
+++ apps/preview/.storybook/main.ts
@@ -1,20 +1,27 @@
+import { createRequire } from "node:module";
+import { dirname, join } from "node:path";
 import type { StorybookConfig } from "@storybook/react-vite";
+
+const require = createRequire(import.meta.url);
 
 const config: StorybookConfig = {
 	framework: {
-		name: "@storybook/react-vite",
+		name: getAbsolutePath("@storybook/react-vite"),
 		options: {},
 	},
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 	addons: [
-		"@storybook/addon-essentials",
-		"@storybook/addon-a11y",
-		"@storybook/addon-interactions",
-		"@storybook/addon-themes",
-	],
+        getAbsolutePath("@storybook/addon-a11y"),
+        getAbsolutePath("@storybook/addon-themes"),
+        getAbsolutePath("@storybook/addon-docs")
+    ],
 	docs: {
 		defaultName: "Docs",
 	},
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+    return dirname(require.resolve(join(value, "package.json")));
+}
--- apps/preview/eslint.config.js
+++ apps/preview/eslint.config.js
@@ -1,9 +1,9 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { config as reactInternal } from "@repo/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
-export default [
-	...reactInternal,
-	{
-		ignores: ["dist/**"],
-	},
-];
+export default [...reactInternal, {
+    ignores: ["dist/**"],
+}, ...storybook.configs["flat/recommended"]];
