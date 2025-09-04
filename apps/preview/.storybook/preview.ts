7299494f51f3c1e33cdcb6ade6637ba37ab65b59
serudda <sergioruizdavila@gmail.com>
2025-09-04 13:47:12 -0500

    feat(preview): update Storybook configuration, add onboarding addon, and include custom theme with logo

--- apps/preview/.storybook/preview.ts
+++ apps/preview/.storybook/preview.ts
@@ -1,9 +1,39 @@
 import type { Preview } from "@storybook/react-vite";
+import { themes } from 'storybook/theming';
 import "../src/index.css";
 
 const preview: Preview = {
 	parameters: {
-		controls: { expanded: true },
+		docs: {
+			theme: themes.dark,
+		},
+		backgrounds: {
+      default: 'black',
+      values: [
+        {
+          name: 'black',
+          value: '#000000',
+        },
+        {
+          name: 'white',
+          value: '#FFFFFF',
+        },
+      ],
+    },
+
+		options: {
+      storySort: {
+        order: ['Overview', 'Foundations', 'UI Components', '*'],
+      },
+    },
+
+		controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+			expanded: true,
+    },
 		actions: { argTypesRegex: "^on[A-Z].*" },
 		layout: "centered",
 	},
---
+++ apps/preview/.storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from 'storybook/theming';
+import logo from './logo.svg';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Side UI',
+  brandUrl: 'https://side.ui/',
+  brandImage: logo,
+  brandTarget: '_self',
+});
